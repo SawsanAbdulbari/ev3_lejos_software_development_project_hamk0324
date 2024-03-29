1ce49a6224bcc31bb4344a7589e0145327d5c22c
LindaMarin <linda23003@student.hamk.fi>
2024-03-29 18:42:12 +0200

    Added MotorControl and edited MainClass

---
+++ EV3LeJOSProjectTeam17_0324/src/main.java.motors/MotorControl.java
@@ -0,0 +1,62 @@
+package motors;
+
+import lejos.hardware.port.MotorPort;
+import lejos.utility.Delay;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class MotorControl implements Runnable
+{
+
+	private static EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+	private static EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+	
+	private static int baseSpeed = 100;
+
+    
+    @Override
+	public void run() 
+	{
+		while(true)
+		{
+			forward();
+			turnLeft();
+			turnRight();
+		}
+	}
+
+	
+    public static void forward() 
+    {
+        leftMotor.setSpeed(baseSpeed);
+        rightMotor.setSpeed(baseSpeed);
+        leftMotor.forward();
+        rightMotor.forward();
+        Delay.msDelay(2000);
+    }
+    public static void turnLeft() 
+    {	
+        double leftSpeed = baseSpeed * 0.8;
+        double rightSpeed = baseSpeed * 1.2;
+        leftMotor.setSpeed((int) leftSpeed);
+        rightMotor.setSpeed((int) rightSpeed);
+        Delay.msDelay(2000);
+    }
+    public static void turnRight() 
+    {
+        double leftSpeed = baseSpeed * 1.5;
+        double rightSpeed = baseSpeed * 0.8;
+        leftMotor.setSpeed((int) leftSpeed);
+        rightMotor.setSpeed((int) rightSpeed);
+        Delay.msDelay(2000);
+    }
+    public static void stop() 
+    {
+        leftMotor.stop();
+        rightMotor.stop();
+    }
+    public static void closeMotors() 
+    {
+        leftMotor.close();
+        rightMotor.close();
+    }
+}
--- EV3LeJOSProjectTeam17_0324/src/main/java/colorSensor/MainClass.java
+++ EV3LeJOSProjectTeam17_0324/src/main/java/colorSensor/MainClass.java
@@ -1,6 +1,7 @@
 package main.java.colorSensor;
 
 import lejos.hardware.Button;
+import motors.MotorControl;
 
 /**
  * 
@@ -13,10 +14,14 @@ public class MainClass
     public static void main(String[] args) 
     {
         ColorSensor colorSensor = new ColorSensor();
+        MotorControl motorControl = new MotorControl();
 
         Thread colorSensorThread = new Thread(colorSensor);
+        Thread motorControlThread = new Thread(motorControl);
 
         colorSensorThread.start();
+        motorControlThread.start();
+
 
         while (true) 
         {
